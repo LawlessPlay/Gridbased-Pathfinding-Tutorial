05dc9badfdf9cfecdad9b31d2d9c12e0dfbae2cb
Aaron Conroy <lawlessplays@gmail.com>
2022-02-13 21:30:52 +0000

    Unused Images

--- Assets/Images/Isometric.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="Isometric" tilewidth="32" tileheight="32" tilecount="1" columns="1">
- <image source="../../Documents/My project/Assets/Images/GrassTile.png" width="32" height="32"/>
-</tileset>
